98f8f4a599071b2be7a5a1083a7abc4274430702
Adejosh01 <adelobajoshua19@gmail.com>
2025-01-03 21:13:11 +0100

    fixing responsiveness

--- src/components/LandingPage/Glow.tsx
+++ src/components/LandingPage/Glow.tsx
@@ -5,8 +5,8 @@ const Glow = () => {
         <div>
             <div className="flex flex-col justify-between items-center bg-[#5e25cc] relative w-full h-[500px] px-6 py-12 sm:px-12 md:px-24 lg:px-32">
                 {/* Testimonial Content */}
-                <div className="flex flex-col gap-6 sm:gap-8 md:gap-10 items-center w-full text-center mt-10">
-                    <span className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold leading-tight text-white w-[1200px] px-4">
+                <div className="flex flex-col gap-6 sm:gap-8 md:gap-10 items-center w-full text-center mt-24">
+                    <span className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold leading-tight text-white lg:w-[1200px] px-4">
                         D'Bellas Glow is my go-to for relaxation and beauty. Highly recommend!
                     </span>
                     {/* User Info */}
